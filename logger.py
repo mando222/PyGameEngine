3d5a91efc6d61cab186c63398d92d1e27e875b02
mando222 <eric.zietlow@datastax.com>
2019-07-14 22:36:03 -0400

    testing gitignore file

--- logger.py
+++ logger.py
@@ -20,4 +20,4 @@ def initLogging(log_file, log_level, logging_mode):
                             level=log_level)
 
     logging.info("_______________________NEW RUN_______________________")
-    logging.info("Logger Initalized at "+log_level)
+    logging.info("Logger Initalized at %s", log_level)
