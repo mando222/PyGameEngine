83fc4f96a5ed18b88da3652b29d026e90694c38a
mando222 <eric.zietlow@datastax.com>
2019-07-17 12:40:34 -0400

    added some preliminary code to make a unit and faction

---
+++ PyGameEngine/createAssets.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import pygame
+from pygame.locals import *
+from logger import *
+from units import *
+from assetLoad import *
+import ConfigParser
+import uuid
+
+def create_unit(faction, name):
+    fileName = faction+'.fac'
+    HERE = Path(__file__).parent.resolve()
+    PATH = HERE / 'factions'/ fileName
+    faction_file = ConfigParser.RawConfigParser()
+    faction_file.read(fileName)
+    if faction_file.get(name, name).exists(): # check if the units section has been made
+        logging.info("error unit %s already exists", name)
+        logging.info("Need to impliment error handling here")
+    else:
+        faction_file.add_section(name)
+        faction_file.set(name, 'name', name)
+        unit_id = uuid.uuid4().hex
+        faction_file.set(name, 'id', unit_id)
+        with open(fileName, 'wb') as file:
+            faction_file.write(file)
+        logging.info("Unit %s Created", name)
+
+def create_faction(name):
+    fileName = name+'.fac'
+    HERE = Path(__file__).parent.resolve()
+    PATH = HERE / 'factions' / fileName
+    print PATH
+    if PATH.exists():
+        logging.info("error faction file for %s already exists", name)
+        logging.info("Need to impliment error handling here")
+    else:
+        print 'creating faction file'
+        config = ConfigParser.RawConfigParser()
+        config.add_section('faction_meta')
+        faction_id = uuid.uuid4().hex
+        config.set('faction_meta', 'name', name)
+        config.set('faction_meta', 'id', faction_id)
+        config.set('faction_meta', 'points', '1000')
+        with open(fileName, 'wb') as file:
+            config.write(file)
+        logging.info("Faction %s Created", name)
+
+#testing code need to remove once done
+#create_faction('testfaction')
+#create_unit('testunit', 'testfaction')
