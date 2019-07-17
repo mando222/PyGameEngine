c7d5d98b360714f6fb2fa3fe494b5316b6275d81
mando222 <eric.zietlow@datastax.com>
2019-07-17 13:22:28 -0400

    added comments to the createAssets.py file

--- PyGameEngine/createAssets.py
+++ PyGameEngine/createAssets.py
@@ -7,9 +7,16 @@ from units import *
 from assetLoad import *
 import ConfigParser
 import uuid
+from unit_stat_values import *
 
-def create_unit(faction, name):
-    fileName = faction+'.fac'
+
+##########################################
+# creates a unit entry in a faction file
+# factino_name is the name of the faction
+# name is the name of the unit being created
+##########################################   
+def create_unit(faction_name, name):
+    fileName = faction_name+'.fac'
     HERE = Path(__file__).parent.resolve()
     PATH = HERE / 'factions'/ fileName
     faction_file = ConfigParser.RawConfigParser()
@@ -22,12 +29,21 @@ def create_unit(faction, name):
         faction_file.set(name, 'name', name)
         unit_id = uuid.uuid4().hex
         faction_file.set(name, 'id', unit_id)
+        faction_file.set(name, 'point_alowance', points.UNIT_ALLOWANCE)
         with open(fileName, 'wb') as file:
             faction_file.write(file)
         logging.info("Unit %s Created", name)  
 
-def edit_unit(action, faction, name, tag, value='none'):
-    fileName = faction+'.fac'
+##########################################
+# edits a unit entry in a faction file
+# action is the type of operation to do
+# faction_name is the name of the faction that owns the unit
+# name is the name of the unit
+# tag is the name of the atribute to be edited
+# value='none' if aplicable is the value to set the tag to
+##########################################  
+def edit_unit(action, faction_name, name, tag, value='none'):
+    fileName = faction_name+'.fac'
     HERE = Path(__file__).parent.resolve()
     PATH = HERE / 'factions'/ fileName
     faction_file = ConfigParser.RawConfigParser()
@@ -46,26 +62,30 @@ def edit_unit(action, faction, name, tag, value='none'):
         logging.info("error unit %s doesn't exist", name)
         logging.info("Need to impliment error handling here")
 
-def create_faction(name):
-    fileName = name+'.fac'
+##########################################
+# creates a faction file
+# factino_name is the name of the faction
+##########################################   
+def create_faction(faction_name):
+    fileName = faction_name+'.fac'
     HERE = Path(__file__).parent.resolve()
     PATH = HERE / 'factions' / fileName
     print PATH
     if PATH.exists():
-        logging.info("error faction file for %s already exists", name)
+        logging.info("error faction file for %s already exists", faction_name)
         logging.info("Need to impliment error handling here")
     else:
         print 'creating faction file'
         config = ConfigParser.RawConfigParser()
         config.add_section('faction_meta')
         faction_id = uuid.uuid4().hex
-        config.set('faction_meta', 'name', name)
+        config.set('faction_meta', 'name', faction_name)
         config.set('faction_meta', 'id', faction_id)
-        config.set('faction_meta', 'points', '1000')
+        config.set('faction_meta', 'points', points.FACTION_ALLOWANCE)
         with open(fileName, 'wb') as file:
             config.write(file)
-        logging.info("Faction %s Created", name)
+        logging.info("Faction %s Created", faction_name)
 
 #testing code need to remove once done
-#create_faction('testfaction')
+create_faction('testfaction')
 #create_unit('testunit', 'testfaction')
---
+++ PyGameEngine/unit_stat_values.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class points(Enum):
+    # Point allowances
+    UNIT_ALLOWANCE = 100
+    FACTION_ALLOWANCE = 1000
+    
+    # Point costs
+    TESTABILITY1_COST = 1
+    TESTABILITY2_COST = 1
+    TESTABILITY3_COST = 1
+    TESTABILITY4_COST = 1
+    TESTABILITY5_COST = 1
+    TESTABILITY6_COST = 1
+    TESTABILITY7_COST = 1
+    TESTABILITY8_COST = 1
+    TESTABILITY9_COST = 1
+    
