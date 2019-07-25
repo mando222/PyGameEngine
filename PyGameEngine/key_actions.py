293f0073b5ee429275143effa9c245863eb8a157
mando222 <eric.zietlow@datastax.com>
2019-07-25 13:05:46 -0400

    key_actions.py had some function calls with no returns

--- PyGameEngine/key_actions.py
+++ PyGameEngine/key_actions.py
@@ -16,12 +16,12 @@ def key_event(key, unit_list, key_bindings):
         elif (key == key_bindings.TEST_SPAWN_RIGHT):
             unit_list = add_units(unit_list, 'unit')
         elif (key == key_bindings.CAMERA_DOWN):
-            unit_list = move_units(unit_list, 'S', unit_list[0].instanceID)
+            move_units(unit_list, 'S', unit_list[0].instanceID)
         elif (key == key_bindings.CAMERA_UP):
-            unit_list = move_units(unit_list, 'N', unit_list[0].instanceID)
+            move_units(unit_list, 'N', unit_list[0].instanceID)
         elif (key == key_bindings.CAMERA_RIGHT):
-            unit_list = move_units(unit_list, 'E', unit_list[0].instanceID)
+            move_units(unit_list, 'E', unit_list[0].instanceID)
         elif (key == key_bindings.CAMERA_LEFT):
-            unit_list = move_units(unit_list, 'W', unit_list[0].instanceID)
+            move_units(unit_list, 'W', unit_list[0].instanceID)
         elif (key == key_bindings.DELETE or key_bindings.ALT_DELETE):
-            unit_list = delete_units(unit_list, unit_list[0].instanceID)
+            delete_units(unit_list, unit_list[0].instanceID)
